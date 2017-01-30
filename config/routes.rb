5b309a1ca1b44b06eed76f0006b7df6b6c614a2c
Ioannis Poulakas <giannis.p@gmail.com>
2017-01-30 23:50:50 +0200

    Start with boilerplate

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home/index'
+  get 'home/timestamp'
+
+  root 'home#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
