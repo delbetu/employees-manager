dbf2cc1913603541a7594fc647382718da697fcd
M.Bellucci <delbetu@gmail.com>
2020-03-04 16:35:14 -0300

    Run pending migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_17_050131) do
+ActiveRecord::Schema.define(version: 2020_03_04_184429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "employees", force: :cascade do |t|
+    t.string "name"
+    t.string "last_name"
+    t.string "gender"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
