5afa5dd682656f21b7385945e75dcba752d70546
M.Bellucci <delbetu@gmail.com>
2020-03-04 16:35:31 -0300

    Add navigation menu

--- front/js/app.js
+++ front/js/app.js
@@ -6,6 +6,7 @@ import '@babel/polyfill';
 
 import React from 'react';
 import ReactDom from 'react-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 // Include the main scss file for webpack processing.
 import '../css/app.scss';
@@ -15,10 +16,36 @@ import getLogger from './utils/logger';
 
 const log = getLogger('App');
 
+const CreateEmployee = () => <h1>Create Employee Component</h1>;
+
 const init = () => {
   log.info('init() :: App starts booting...');
+  ReactDom.render(
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/employees/new">New Employee</Link>
+            </li>
+          </ul>
+        </nav>
 
-  ReactDom.render(<App />, document.getElementById('app'));
+        <Switch>
+          <Route path="/employees/new">
+            <CreateEmployee />
+          </Route>
+          <Route path="/">
+            <App />
+          </Route>
+        </Switch>
+      </div>
+    </Router>,
+    document.getElementById('app'),
+  );
 };
 
 // init app
