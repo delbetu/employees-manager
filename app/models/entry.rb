348ffdf163dae73cd30c18448c8a0994fd984e45
M.Bellucci <delbetu@gmail.com>
2020-03-05 15:02:02 -0300

    Add total_time to entry model

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,9 @@
 class Entry < ApplicationRecord
   belongs_to :employee
+
+  # returns number of hours between time in and time out
+  def total_time
+    return 0 unless time_out
+    (time_out - time_in)/60/60
+  end
 end
